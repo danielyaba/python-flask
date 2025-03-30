eb0a4e3c27e5ca95f2f4dbd6abf079eefeb182a5
Daniel Yaba <daniel.yaba@controlup.com>
2025-03-30 21:11:40 +0300

    modified app.py

--- src/app.py
+++ src/app.py
@@ -12,4 +12,8 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(debug=os.getenv("FLASK_DEBUG", "False").lower() == "true")
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=os.getenv("FLASK_DEBUG", "False").lower() == "true",
+    )
